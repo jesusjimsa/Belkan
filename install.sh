7e176cc9c257ff191d99fd590cb458be49d69bcf
jesusjimsa <jesusjimsa@icloud.com>
2017-05-21 18:12:34 +0200

    Limpieza de archivos proporcionados por el profesor

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install -y freeglut3 freeglut3-dev libjpeg-dev libopenmpi-dev openmpi-bin openmpi-doc libxmu-dev libxi-dev cmake libboost-all-dev 
--- src/modelos/leo/brazo.cpp
+++
@@ -1,6 +0,0 @@
-#include "modelos/leo/brazo.hpp"
-
-
-BrazoLeo3D::BrazoLeo3D() : Objeto3D(){
-  
-}
